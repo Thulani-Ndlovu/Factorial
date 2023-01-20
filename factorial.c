086a3d3396f187ea722865e1f617059aaa9f1bf2
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-01-20 17:45:38 +0200

    Factorial

--- factorial.c
+++ factorial.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int fact(int num);
 int main(void)
 {
-
-
+  int number =6;
+   int factorial=fact(number);
+   printf("%d! is equal to %d\n",number,factorial);
     return 0;
+}
+
+int fact(int num)
+{
+
+  if(num==0 || num==1)
+  {
+    return 1;
+  }
+ else
+    {return num*fact(num-1);};
+  
 }
